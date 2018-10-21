7b39156a8e88284805c01a0f0fc29482962f0495
Martin Keßler <martin@moegger.de>
2018-10-21 18:52:14 +0200

    use system colors for drawing studio-clock face

--- voctogui/lib/studioclock.py
+++ voctogui/lib/studioclock.py
@@ -36,30 +36,40 @@ class StudioClock(Gtk.ToolItem):
 
     # override drawing of the widget
     def do_draw(self, cr):
+        context = self.get_style_context()
+
+        flags = Gtk.StateFlags.NORMAL
+        color_normal = context.get_color(flags)
+        border_color_normal = context.get_border_color(flags)
+        background_color_normal = context.get_background_color(flags)
+        font_normal = context.get_font(flags).get_family()
+
+        flags = Gtk.StateFlags.SELECTED
+        color_selected = context.get_color(flags)
+        border_color_selected = context.get_border_color(flags)
+        background_color_selected = context.get_background_color(flags)
+
+        #rgba_on = tuple(background_color_selected)
+        #rgba_off = tuple(color_normal)
+
+        rgba_on = tuple(color_normal)
+        rgba_off = tuple(background_color_normal)
+
         # get actual widget size
         width = self.get_allocated_width()
         height = self.get_allocated_height()
         # calculate center and radius of the clock
         center = (width / 2, height / 2)
         radius = min(center)
-        # setup gradients for clock background to get a smooth border
-        bg_lg = cairo.RadialGradient(
-            center[0], center[1], 0, center[0], center[1], radius)
-        bg_lg.add_color_stop_rgba(0.0, 0, 0, 0, 1.0)
-        bg_lg.add_color_stop_rgba(0.9, 0, 0, 0, 1.0)
-        bg_lg.add_color_stop_rgba(1.0, 0, 0, 0, 0.0)
-        # paint background
-        cr.set_source(bg_lg)
-        cr.arc(center[0], center[1], radius, 0, 2 * math.pi)
-        cr.fill()
         local_time = time.localtime(self.last_draw_time)
         # draw ticks for every second
         for tick in range(0, 60):
             # fade out seconds in future and highlight past seconds
-            if tick > local_time.tm_sec:
-                cr.set_source_rgb(0.2, 0.3, 0.01)
-            else:
-                cr.set_source_rgb(0.764, 0.804, 0.176)
+            if tick <= local_time.tm_sec:
+                cr.set_source_rgba(*rgba_on)
+            elif rgba_off:
+                cr.set_source_rgba(*rgba_off)
+
             # calculate tick position
             angle = tick * math.pi / 30
             pos = (center[0] + math.sin(angle) * radius * 0.8,
@@ -68,7 +78,7 @@ class StudioClock(Gtk.ToolItem):
             cr.arc(pos[0], pos[1], radius / 40, 0, 2 * math.pi)
             cr.fill()
         # draw persistant ticks every five seconds
-        cr.set_source_rgb(0.764, 0.804, 0.176)
+        cr.set_source_rgba(*rgba_on)
         for tick in range(0, 12):
             # calculate tick position
             angle = tick * math.pi / 6
@@ -79,6 +89,7 @@ class StudioClock(Gtk.ToolItem):
             cr.fill()
         # set a reasonable font size
         cr.set_font_size(cr.user_to_device_distance(0, height / 5)[1])
+        cr.select_font_face(font_normal, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
         # format time into a string
         text = "{:02d}:{:02d}".format(local_time.tm_hour, local_time.tm_min)
         # get text drawing extents
